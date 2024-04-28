6be3388d5b4093aa2673a701bcf40e8749bf7268
Teren <Official.Teren@yandex.ru>
2024-04-28 17:59:25 +0300

    zxc

--- main.cpp
+++ main.cpp
@@ -5,11 +5,7 @@ int main()
 {
 	for (int i = 0; i <= 10; i++)
 	{
-<<<<<<< HEAD
 		cout << "×èñëî ðàâíî = " << i << endl;
-=======
-		cout << " Ð§Ð¸ÑÐ»Ð¾ Ñ€Ð°Ð²Ð½Ð¾ = " << i << endl;
->>>>>>> 3235e04bb448f5f9fbfa2fa93efbb4a76c03fe51
 		if (i == 7)
 		{
 			break;
